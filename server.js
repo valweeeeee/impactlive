701a64de5ad9db8d10c3c4fbcc763b877d013b0a
Valerie Myers <vmyers@salesforce.com>
2018-12-03 17:09:11 -0500

    file uploads

--- server.js
+++ server.js
@@ -212,7 +212,6 @@ app.post('/newrecord/', (req,res,next)=>{
         return false;
       }
     else{
-      console.log(results);
       var newlyCreatedPresentationId = results.presentationid;
       return res.json({'data':results.presentationid});
     }
