bd3d81ed4f21aef1562b5d1c3f13608f91bf2abb
Valerie Myers <vmyers@salesforce.com>
2018-12-05 09:41:00 -0500

    file uploads

--- server.js
+++ server.js
@@ -219,7 +219,6 @@ app.post('/newrecord/', (req,res,next)=>{
     }
   });
 });
-/*
 function updateRecord(req,presentationid,res){
     sess=req.session;
         let query = "UPDATE presentations SET companylogourl='"+req.body.companylogourl+"',pushcontent1url='"+req.body.pushcontent1url+"',pushcontent2url='"+req.body.pushcontent2url+"', pushcontent3url='"+req.body.pushcontent3url+"',pushcontent4url='"+req.body.pushcontent4url+"',pushcontent5url='"+req.body.pushcontent5url+"'  where userid='"+req.body.userid+ "' AND presentationid='"+presentationid+"'";
@@ -241,7 +240,6 @@ function updateRecord(req,presentationid,res){
 
       }
 
-*/
 
 app.post('/delete/', (req,res,next)=>{
   sess=req.session;
