d7af2440fd5cb09cb469b6ef5986fb5af1d471e6
Valerie Myers <vmyers@salesforce.com>
2019-01-14 13:15:10 -0500

    file uploads

--- server.js
+++ server.js
@@ -63,109 +63,11 @@ const DB = {
     }
 }
 /* Data Functions*/
-app.post('/loginpage/', (req, res, next) => {
-    sess=req.session;
-    let query = "SELECT userid from users where username='"+req.body.username + "' AND password=CRYPT('" + req.body.password + "',password)";
-    //console.log(query);
-    DB.query(query, (err, results) => {
-      if (results && results != ''){
-        sess.userid=results[0].userid;
-        res.json({ "ok": sess.userid });
-      }
-      else{
-        res.json({ "ok": "false" });
-      }
-    })
-
-});
-app.get('/presentations/', (req, res, next) => {
-    sess=req.session;
-});
 app.post('/getpresentations/', (req,res,next)=>{
-  let query = "SELECT * from presentations where userid='"+req.body.userid+ "' ORDER BY dateCreated DESC";
+  let query = "SELECT * from presentations";
   console.log(query);
 
   DB.query(query, (err, results) => {
     res.send(results);
-  });
-});
-app.post('/getpresentation/', (req,res,next)=>{
-  let query = "SELECT * from presentations where presentationid='"+req.body.presentationid+"' ORDER BY dateCreated DESC";
-  DB.query(query, (err, results) => {
-    if (results && results != ''){
-      res.send(results);
-    }
-    else{
-      res.send('0');
-    }
-  });
-});
-app.post('/makeedits/', (req,res,next)=>{
-  sess=req.session;
-  let query = "Update presentations SET companyname='"+req.body.companyname+"',email1subject='"+req.body.email1subject+"',companylogourl='"+req.body.companylogourl+"',pushcontent1url='"+req.body.pushcontent1url+"',pushcontent2url='"+req.body.pushcontent2url+"',pushcontent3url='"+req.body.pushcontent3url+"',pushcontent4url='"+req.body.pushcontent4url+"',pushcontent5url='"+req.body.pushcontent5url+"' ,surveylink='"+req.body.surveylink+"' where userid='"+req.body.userid+ "' AND presentationid='"+req.body.presentationid+"'";
-  console.log(query);
-  DB.query(query, (err, results) => {
-    if(err){
-      res.json({ "ok": "false" });
-    }
-    else{
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,    Accept");
-      res.json({"data":'ok'},200);
-    }
-  });
-});
-app.post('/newrecord/', (req,res,next)=>{
-  sess=req.session;
-
-  let query = "INSERT INTO presentations (companyname,email1subject,surveylink,userid,datecreated) VALUES ('"+req.body.companyname+"','"+req.body.email1subject+"','"+req.body.surveylink+"','"+req.body.userid+"', NOW()) RETURNING presentationid";
-  console.log(query);
-  DB.query(query, (err, results) => {
-    if(err) {
-        return res.json({'err':1,'message':'ERROR'});
-      }
-    else{
-      //console.log(results);
-      presentationid=results[0].presentationid;
-      console.log(presentationid);
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,    Accept");
-      res.json({"data":presentationid},200);
-    }
-  });
-});
-function updateRecord(req,presentationid,res){
-    sess=req.session;
-        let query = "UPDATE presentations SET companylogourl='"+req.body.companylogourl+"',pushcontent1url='"+req.body.pushcontent1url+"',pushcontent2url='"+req.body.pushcontent2url+"', pushcontent3url='"+req.body.pushcontent3url+"',pushcontent4url='"+req.body.pushcontent4url+"',pushcontent5url='"+req.body.pushcontent5url+"'  where userid='"+req.body.userid+ "' AND presentationid='"+presentationid+"'";
-        //console.log(query);
-        DB.query(query, (err, results) => {
-          if(err) {
-              res.json({'err':1,'message':'ERROR'});
-              return false;
-            }
-            else{
-              console.log('hi');
-
-              res.header("Access-Control-Allow-Origin", "*");
-              res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,    Accept");
-              res.json({"data":presentationid},200);
-            }
-
-          });
-
-      }
-
-
-app.post('/delete/', (req,res,next)=>{
-  sess=req.session;
-  let query = "Delete FROM presentations where userid='"+req.body.userid+ "' AND presentationid='"+req.body.presentationid+"'";
-  DB.query(query, (err, results) => {
-    if(err){
-      res.json({ "ok": "false" });
-    }
-    else{
-      res.json({ "ok": sess.userid });
-    }
-
   });
 });
